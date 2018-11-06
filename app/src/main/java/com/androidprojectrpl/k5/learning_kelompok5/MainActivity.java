77de252a86ffe14ed68fa4784544dc1bb1e6e690
ozianf101 <ahnufasmada01@gmail.com>
2018-11-06 10:04:42 +0700

    tambahan dasar login,,, dan activity lain dengan nav drawer

--- app/src/main/java/com/androidprojectrpl/k5/learning_kelompok5/MainActivity.java
+++ app/src/main/java/com/androidprojectrpl/k5/learning_kelompok5/MainActivity.java
@@ -1,13 +1,25 @@
 package com.androidprojectrpl.k5.learning_kelompok5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.logoLogin);
+        mImageView.setImageResource(R.drawable.logo);
+    }
+
+    public void LOGIN(View view) {
+        Intent intent = new Intent(MainActivity.this, HomeView.class);
+        startActivity(intent);
     }
 }
